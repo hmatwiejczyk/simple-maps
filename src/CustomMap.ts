84e49ba17f163d8b5a9fec4e2f6c05bd0f3f37f9
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-13 23:08:57 +0200

    add mappable interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,9 @@
-import { User } from './User';
-import { Company } from './Company';
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -17,23 +21,13 @@ export class CustomMap {
     );
   }
 
-  addUserMarker(user: User): void {
+  addMarker(marker: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
+        lat: marker.location.lat,
+        lng: marker.location.lng
       }
     });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng
-      }
-    })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const company = new Company();
 
 loadMaps().then(() => {
   const customMap = new CustomMap('map');
-  customMap.addUserMarker(user);
-  customMap.addCompanyMarker(company);
+  customMap.addMarker(user);
+  customMap.addMarker(company);
 });
